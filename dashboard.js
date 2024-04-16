38848cf399a35fdfd5779b814737dc15bb95d98f
Lvc4 <luca.grosshennig@gmx.de>
2024-04-16 21:06:16 +0200

    feat: add load and store logic for firefox

--- dashboard.js
+++ dashboard.js
@@ -2,17 +2,25 @@ var form = document.getElementById("token-form");
 var github_token = document.getElementById("github_token");
 var user_name = document.getElementById("user_name");
 
+var browser = (window.browser) ? window.browser : window.chrome;
 
 load_token();
 
 form.addEventListener('submit', save_token, false);
 
-function load_token(){
-    user_name.value="test user";
-    github_token.value="test token";
+function load_token() {
+    browser.storage.sync.get()
+        .then(data => {
+            github_token.value = data.github_token || ''
+            user_name.value = data.user_name || ''
+        }, error => {
+            console.error(`Error: ${error}`)
+        })
 }
 
-function save_token(){
-    console.log(github_token.value);
-    console.log(user_name.value);
+function save_token() {
+    browser.storage.sync.set({
+        github_token: github_token.value,
+        user_name: user_name.value,
+    })
 }
